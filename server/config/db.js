e42dbd5270f0ab933606bbc96aba08afeb6ef10f
abderrahim boudounit <abderrahimboudounit@gmail.com>
2024-11-20 17:54:29 +0100

    configuring db connection

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: 'localhost',
-  database: 'api',
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.PORT,
 });
