5c032b838fd403aa24219264155aa40e34828b26
azedine ouhadou <aouhadou@student.1337.ma>
2024-11-22 19:30:29 +0100

    add atomic design structure

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,9 @@
+import CustomButton from "@/components/atoms/Button";
+
 export default function Home() {
-  return <div className="container mx-auto">matcha</div>;
+  return (
+    <div className="container mx-auto">
+      <CustomButton />
+    </div>
+  );
 }
---
+++ client/components/atoms/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const CustomButton = () => {
+  return (
+    <Button variant="outline" className="bg-primary">
+      Button
+    </Button>
+  );
+};
+
+export default CustomButton;
