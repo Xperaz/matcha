711be642fb6a1d92a07538b012ba5f9ee39e9a5a
Azedine Ouhadou <45069518+Xperaz@users.noreply.github.com>
2024-11-23 12:35:01 +0100

    Feat/setup UI library (#2)
    * add atomic design structure
    
    * add reausable btns

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import { ButtonWithIcon } from "@/components/atoms/ButtonWithIcon";
+import { ButtonLoading } from "@/components/atoms/LoadingButton";
+import { Button } from "@/components/ui/button";
+import { Mail } from "lucide-react";
+
 export default function Home() {
-  return <div className="container mx-auto">matcha</div>;
+  return (
+    <div className="container mx-auto flex gap-4">
+      <Button variant="default" onClick={() => alert("default")}>
+        Primary
+      </Button>
+      <Button variant="outline" onClick={() => alert("outlined")}>
+        Outlined
+      </Button>
+      <Button variant="destructive" onClick={() => alert("delete")}>
+        Delete
+      </Button>
+      <ButtonLoading title="Send Request" />
+      <ButtonWithIcon variant="link" onClick={() => alert("link")}>
+        <Mail /> Login with Email
+      </ButtonWithIcon>
+    </div>
+  );
 }
