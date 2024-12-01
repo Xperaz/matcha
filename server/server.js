1ef2348e68a5a99f6f83a55fa27e390a8179d788
abderrahim boudounit <abderrahimboudounit@gmail.com>
2024-12-01 23:50:59 +0100

    adding swagger

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenvt from "dotenv";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import fs from 'fs';
 
 //routes
 import authRoutes from "./routes/authRoutes.js";
@@ -12,6 +15,14 @@ const port = process.env.SERVER_PORT || 5000;
 
 app.use(express.json()); //middelware to parse incoming requests
 app.use(cookieParser()); // for parssing cookies
+
+
+// adding swagger
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve("./swagger.json"), "utf8")
+);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use("/api/auth", authRoutes);
 
