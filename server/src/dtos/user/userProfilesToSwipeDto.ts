1d1cfff299cac87a3034f9983408495054854103
abderrahim boudounit <abderrahimboudounit@gmail.com>
2024-12-11 23:51:36 +0100

    adding function to get users profile to choose from

---
+++ server/src/dtos/user/userProfilesToSwipeDto.ts
@@ -0,0 +1,8 @@
+export interface UserProfilesToSwipeDto {
+    id: string;
+    first_name: string;
+    last_name: string;
+    profile_picture: string;
+    gps_position: string;
+    age: number;
+};
