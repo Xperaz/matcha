6a57f91f37425204ede869a0677910b3e27215ae
abderrahim boudounit <abderrahimboudounit@gmail.com>
2025-01-11 22:44:29 +0100

    adding user profile page

--- client/components/organisms/ImagesSection.tsx
+++ client/components/organisms/ImagesSection.tsx
@@ -1,43 +1,63 @@
 import Image from "next/image";
 import { Plus, X } from "lucide-react"; // Import Lucide icons
-import image2 from "@/public/images/home.webp";
-import image3 from "@/public/images/login.jpeg";
-import image4 from "@/public/images/signup.jpg";
-
-const images = [image2, image3, image4, image3];
+import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/constants/query_keys";
+import { UserImages } from "@/types/images";
+import { useEffect, useState } from "react";
+import { getImages } from "@/services/requests/images";
+import { deleteImage } from "@/services/requests/images";
 
 const ImagesSection = () => {
+  const [images, setImages] = useState<UserImages[]>([]);
+
+  const { data, isSuccess, refetch } = useQuery({
+    queryKey: [QUERY_KEYS.images],
+    queryFn: async () => {
+      const retData = await getImages();
+      return retData.data.data;
+    },
+  });
+
+  const handleRemoveImage = async (imageId: string) => {
+    await deleteImage(imageId);
+    await refetch();
+  };
+
+  useEffect(() => {
+    if (data) {
+      setImages(data);
+    }
+  }, [data, isSuccess]);
+
   return (
     <>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
-        {/* Image Cards */}
-        {images.map((image, index) => (
+        {images.map((image) => (
           <div
-            key={index}
-            className="relative group bg-white rounded-lg shadow-md overflow-hidden"
+            key={image.id}
+            className="relative group bg-white rounded-lg  overflow-hidden"
           >
-            <div className="aspect-square relative">
+            <div className="aspect-square ">
               <Image
-                src={image}
-                alt={`Image ${index + 1}`}
+                src={image.secure_url}
+                alt="User image"
                 fill
                 className="object-cover"
               />
             </div>
             <button
+              onClick={() => handleRemoveImage(image.id)}
               className="absolute top-2 right-2 bg-red-500 hover:bg-red-600 text-white p-1.5 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"
-              aria-label="Delete image"
             >
-              <X size={16} /> {/* Lucide X icon */}
+              <X size={16} />
             </button>
           </div>
         ))}
 
         {images.length < 5 && (
-          <div className="aspect-square">
+          <div className="">
             <button className="w-full h-full flex flex-col items-center justify-center border-2 border-dashed border-gray-300 rounded-lg hover:border-gray-400 transition-colors duration-200 gap-2">
               <Plus size={24} className="text-gray-400" />{" "}
-              {/* Lucide Plus icon */}
               <span className="text-sm font-medium text-gray-600">
                 Add Image
               </span>
--- client/components/organisms/UserProfile.tsx
+++ client/components/organisms/UserProfile.tsx
@@ -1,84 +1,125 @@
 "use client";
 
 import Image from "next/image";
-import avatr from "@/public/images/avatar.png";
 import ImagesSection from "./ImagesSection";
+import { IUserType } from "@/types/user";
+import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/constants/query_keys";
+import { getMyProfile } from "@/services/requests/profile";
+import { Loader } from "lucide-react";
+import avatar from "@/public/images/avatar.png";
 
 const ProfileBasicInfo = () => {
+  const [user, setUser] = useState<IUserType>({} as IUserType);
+  const { data, isLoading, isSuccess } = useQuery({
+    queryKey: [QUERY_KEYS.profileData],
+    queryFn: async () => {
+      const retData = await getMyProfile();
+      return retData.data.data;
+    },
+  });
+
+  useEffect(() => {
+    if (data) {
+      setUser(data);
+    }
+  }, [data, isSuccess]);
+
   return (
-    <div className="flex py-4 flex-col gap-8 justify-center mx-auto max-w-4xl">
-      <section className="flex flex-col items-center justify-center">
-        <div className="w-32 h-32 rounded-full bg-center bg-cover bg-green-400 overflow-hidden">
-          <Image alt="profile picture" src={avatr} />
+    <>
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-90">
+          <Loader />
         </div>
-        <h2 className="text-lg font-bold">first name last name</h2>
-        <p className="text-sm text-gray-500 ">country city</p>
-      </section>
-      <section className="mx-4">
-        <h3 className="text-lg font-bold pb-2">Interest</h3>
-        <div className="flex flex-wrap gap-2">
-          <span className="px-4 py-2 bg-gray-200 text-sm rounded-full">
-            Hiking
-          </span>
-          <span className="px-4 py-2 bg-gray-200 text-sm rounded-full">
-            Hiking
-          </span>
-          <span className="px-4 py-2 bg-gray-200 text-sm rounded-full">
-            Hiking
-          </span>
-          <span className="px-4 py-2 bg-gray-200 text-sm rounded-full">
-            Hiking
-          </span>
-          <span className="px-4 py-2 bg-gray-200 text-sm rounded-full">
-            Hiking
-          </span>
+      )}
+
+      {!isLoading && (
+        <div className="flex py-4 flex-col gap-8 justify-center mx-auto max-w-[1000px]">
+          <section className="flex flex-col items-center justify-center">
+            <div className="w-32 h-32 rounded-full bg-center bg-cover overflow-hidden">
+              <Image
+                alt="profile picture"
+                src={user.profile_picture || avatar}
+                width={300}
+                height={300}
+              />
+            </div>
+            <h2 className="text-lg font-bold">
+              {user.first_name} {user.last_name}
+            </h2>
+            <p className="text-sm text-gray-500 ">
+              {user.country} {user.city}
+            </p>
+          </section>
+          <section className="mx-4">
+            <h3 className="text-lg font-bold pb-2">Interest</h3>
+            <div className="flex flex-wrap gap-2">
+              {user.interests?.map((interest) => (
+                <span
+                  key={interest}
+                  className="px-4 py-2 bg-gray-200 text-sm rounded-full"
+                >
+                  {interest}
+                </span>
+              ))}
+            </div>
+          </section>
+          <section className="flex flex-col mx-4">
+            <h3 className="text-lg font-bold">About</h3>
+            <div>
+              <div className="flex justify-between py-2">
+                <span className="text-sm text-gray-500">fame rating</span>
+                <span className="text-sm  w-[300px] overflow-hidden ">
+                  {user.fame_rating}
+                </span>
+              </div>
+              <div className="flex justify-between py-2">
+                <span className="text-sm text-gray-500">biography</span>
+                <span className="text-sm  w-[300px] overflow-hidden ">
+                  {user.biography}
+                </span>
+              </div>
+              <div className="flex justify-between py-2">
+                <span className="text-sm text-gray-500">gender</span>
+                <span className="text-sm  w-[300px] overflow-hidden">
+                  {user.gender}
+                </span>
+              </div>
+              <div className="flex justify-between py-2">
+                <span className="text-sm text-gray-500">
+                  sexual preferences
+                </span>
+                <span className="text-sm  w-[300px] overflow-hidden">
+                  {user.sexual_preferences}
+                </span>
+              </div>
+            </div>
+          </section>
+          <section className="flex flex-col mx-4">
+            <h3 className="text-lg font-bold pb-2">Credentials</h3>
+            <div>
+              <div className="flex justify-between py-2">
+                <span className="text-sm text-gray-500">Email</span>
+                <span className="text-sm  w-[300px] overflow-hidden ">
+                  {user.email}
+                </span>
+              </div>
+              <div className="flex justify-between py-2">
+                <span className="text-sm text-gray-500">password</span>
+                <span className="text-sm w-[300px] overflow-hidden">
+                  *********
+                </span>
+              </div>
+            </div>
+          </section>
+          <section className="mx-4">
+            <h3 className="text-lg font-bold pb-2">Images</h3>
+            <ImagesSection />
+          </section>
         </div>
-      </section>
-      <section className="flex flex-col mx-4">
-        <h3 className="text-lg font-bold">About</h3>
-        <div>
-          <div className="flex justify-between py-2">
-            <span className="text-sm text-gray-500">fame rating</span>
-            <span className="text-sm  w-[300px] overflow-hidden ">12</span>
-          </div>
-          <div className="flex justify-between py-2">
-            <span className="text-sm text-gray-500">biography</span>
-            <span className="text-sm  w-[300px] overflow-hidden ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </span>
-          </div>
-          <div className="flex justify-between py-2">
-            <span className="text-sm text-gray-500">gender</span>
-            <span className="text-sm  w-[300px] overflow-hidden">MALE</span>
-          </div>
-          <div className="flex justify-between py-2">
-            <span className="text-sm text-gray-500">sexual preferences</span>
-            <span className="text-sm  w-[300px] overflow-hidden">FEMAL</span>
-          </div>
-        </div>
-      </section>
-      <section className="flex flex-col mx-4">
-        <h3 className="text-lg font-bold pb-2">Credentials</h3>
-        <div>
-          <div className="flex justify-between py-2">
-            <span className="text-sm text-gray-500">Email</span>
-            <span className="text-sm  w-[300px] overflow-hidden ">
-              email@gmail.com
-            </span>
-          </div>
-          <div className="flex justify-between py-2">
-            <span className="text-sm text-gray-500">password</span>
-            <span className="text-sm w-[300px] overflow-hidden">
-              **********
-            </span>
-          </div>
-        </div>
-      </section>
-      <section className="mx-4">
-        <h3 className="text-lg font-bold pb-2">Images</h3>
-        <ImagesSection />
-      </section>
-    </div>
+      )}
+    </>
   );
 };
 
