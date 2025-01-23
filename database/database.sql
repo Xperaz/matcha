3547c4f747bd41e47663be2ce5b946f568795cb2
abderrahim boudounit <abderrahimboudounit@gmail.com>
2025-01-23 20:28:39 +0100

    syncing databse

--- database/database.sql
+++ database/database.sql
@@ -8,7 +8,7 @@ CREATE TYPE like_status AS ENUM ('LIKED', 'MATCH', 'DISLIKED');
 CREATE TABLE users (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     biography VARCHAR(500),
@@ -26,6 +26,7 @@ CREATE TABLE users (
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     is_active BOOLEAN DEFAULT FALSE,
     profile_completed BOOLEAN DEFAULT FALSE
+    is_google BOOLEAN DEFAULT FALSE,
 );
 
 CREATE TABLE messages (
